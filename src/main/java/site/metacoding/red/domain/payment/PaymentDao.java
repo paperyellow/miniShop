0b96a5d024d74563cbe1e8a6033d9d3a635b11a3
basakk123 <112357424+basakk123@users.noreply.github.com>
2022-11-17 18:15:15 +0900

    결제 내역 전체 보기 완료 (#3)
    * orders dto 만드는 중
    
    * orders, payment 기본 insert 테스트 완료
    
    * 주문하기 완료
    
    * 주문 취소 하는 중
    
    * 주문취소하기 완료, payId => paymentId 로 다 수정
    
    * 결제 내역 전체 보기 완료

--- src/main/java/site/metacoding/red/domain/payment/PaymentDao.java
+++ src/main/java/site/metacoding/red/domain/payment/PaymentDao.java
@@ -1,9 +1,12 @@
 package site.metacoding.red.domain.payment;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import site.metacoding.red.web.dto.PaymentReqDto.PaymentCancelReqDto;
 import site.metacoding.red.web.dto.PaymentReqDto.PaymentInsertReqDto;
+import site.metacoding.red.web.dto.PaymentRespDto.PaymentFindAllRespDto;
 
 @Repository
 public interface PaymentDao {
@@ -11,4 +14,6 @@ public interface PaymentDao {
 	public void insert(PaymentInsertReqDto paymentInsertReqDto);
 
 	public void updateCancel(PaymentCancelReqDto paymentCancelReqDto);
+
+	public List<PaymentFindAllRespDto> findAll();
 }
